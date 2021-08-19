ba184377abb8f86b5555a88dd036a0ba16c5f95e
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-19 17:01:22 +0530

    Retrieve The Data From Database

---
+++ Employee_Payroll/src/main/java/EmployeePayroll.java
@@ -0,0 +1,11 @@
+import lombok.*;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+
+public class EmployeePayroll {
+      private int id;
+     private String name;
+    private double salary;
+}
