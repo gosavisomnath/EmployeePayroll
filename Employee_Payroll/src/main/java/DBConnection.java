e1c8709ce8ae900614e0372b88fb0835673d771e
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-19 16:50:39 +0530

    Connecting To Database

--- Employee_Payroll/src/main/java/DBConnection.java
+++ Employee_Payroll/src/main/java/DBConnection.java
@@ -1,5 +1,40 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+
+
 public class DBConnection {
     public static void main(String[] args) {
-        System.out.println("Welcome To The Employee Payroll Program");
+        String jdbcURL="jdbc:mysql//localhost:3306/payroll_service?useSSL=false";
+        String userName="root";
+        String password="";
+        Connection con;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to database:"+jdbcURL);
+
+            con= DriverManager.getConnection(jdbcURL,userName,password);
+            System.out.println("Connection is Successful"+con);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+    private static void listDrivers(){
+        Enumeration<Driver> driverList=DriverManager.getDrivers();
+        while(driverList.hasMoreElements()){
+            Driver driverClass=driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
     }
 }
